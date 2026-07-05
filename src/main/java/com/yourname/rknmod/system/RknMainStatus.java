5373c94bba189eb07995d235500c2f460288a7f0
Kats676 <jastinbobik83@gmail.com>
2026-07-05 20:44:26 +0400

    Create RknMainStatus.java

---
+++ src/main/java/com/yourname/rknmod/system/RknMainStatus.java
@@ -0,0 +1,6 @@
+package com.yourname.rknmod.system;
+
+public class RknMainStatus {
+    // Если true, то РКН удален и все ограничения в мире снимаются
+    public static boolean isRknDeleted = false;
+}
